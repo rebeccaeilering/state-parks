33aa6518348f3e4d292e0c7f46d6314dc02cdea4
Rebecca Eilering <rebeccaeilering@gmail.com>
2020-11-02 21:30:07 -0600

    added the sign up and login form js and fixed some errors in the main js file

--- public/main.js
+++ public/main.js
@@ -12,27 +12,36 @@ for (let item of anchorlinks) {
     })
 }
 
-const pageTitleText = document.querySelector('.page-title').textContent;
-const headerTitle = document.querySelector('.header-title');
-const headingTitle = document.createElement('h1');
-headingTitle.textContent = pageTitleText;
-headerTitle.appendChild(headingTitle);
-
-const contenth2 = document.querySelector('.details .page-title')
-contenth2.innerHTML = `About ${contenth2.innerHTML}`;
-
-const activitiesString = document.querySelector('.site-activities').textContent;
-const activitiesArray = activitiesString.split(', ');
-const listElement = document.createElement('ul');
-const listContainer = document.querySelector('.activities-list');
-listContainer.appendChild(listElement);
-for (let i = 0; i < activitiesArray.length; i++) {
-  const listItem = document.createElement('li');
-  listItem.innerHTML = activitiesArray[i];
-  listElement.appendChild(listItem);
+const pageTitle = document.querySelector('.page-title');
+if (pageTitle) {
+  const pageTitleText = pageTitle.textContent;
+  const headerTitle = document.querySelector('.header-title');
+  const headingTitle = document.createElement('h1');
+  headingTitle.textContent = pageTitleText;
+  headerTitle.appendChild(headingTitle);
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-  var elem = document.querySelector('.site-activities');
-  elem.parentNode.removeChild(elem);
-});
+const contenth2 = document.querySelector('.details .page-title')
+if (contenth2) {
+  const h2title = contenth2.innerHTML
+  contenth2.innerHTML = `About ${h2title}`;
+}
+
+const siteActivities = document.querySelector('.site-activities')
+if (siteActivities) {
+  const activitiesString = siteActivities.textContent;
+  const activitiesArray = activitiesString.split(', ');
+  const listElement = document.createElement('ul');
+  const listContainer = document.querySelector('.activities-list');
+  listContainer.appendChild(listElement);
+  for (let i = 0; i < activitiesArray.length; i++) {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = activitiesArray[i];
+    listElement.appendChild(listItem);
+  }
+
+  document.addEventListener("DOMContentLoaded", function(){
+    var elem = document.querySelector('.site-activities');
+    elem.parentNode.removeChild(elem);
+  });
+}
