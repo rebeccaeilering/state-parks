9d19d471c401935b934b588ef3e139f72bf21a78
Rebecca Eilering <rebeccaeilering@gmail.com>
2020-11-04 00:09:45 -0600

    added cookie code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var sassMiddleware = require('node-sass-middleware');
 const mongoose = require('mongoose');
 const siteRoutes = require('./routes/siteRoutes');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const { render } = require('ejs');
 var path = require('path');
@@ -26,6 +27,7 @@ app.use(sassMiddleware({
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
@@ -35,6 +37,22 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
+});
+
+// Cookies
+
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+  res.send('you got the cookies!');
+});
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+
+  res.json(cookies);
 });
 
 // Site Routes
