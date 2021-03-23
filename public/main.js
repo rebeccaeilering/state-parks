531ab9704e813b1da8960977d9d6869a83ffbdbb
Rebecca Eilering <rebeccaeilering@gmail.com>
2021-03-22 22:03:45 -0500

    clean up js some and activities in columns

--- public/main.js
+++ public/main.js
@@ -1,34 +1,34 @@
-let anchorlinks = document.querySelectorAll('a[href^="#"]')
+document.addEventListener("DOMContentLoaded", function () {
 
-for (let item of anchorlinks) {
-    item.addEventListener('click', (e)=> {
-        let link = item.getAttribute('href')
-        let target = document.querySelector(link)
-        target.scrollIntoView({
-            behavior: 'smooth'
-        })
-        history.pushState(null, null, link)
-        e.preventDefault()
+  // Smooth scroll
+  let anchorlinks = document.querySelectorAll('a[href^="#"]')
+
+  for (let item of anchorlinks) {
+    item.addEventListener('click', (e) => {
+      let link = item.getAttribute('href')
+      let target = document.querySelector(link)
+      target.scrollIntoView({
+        behavior: 'smooth'
+      })
+      history.pushState(null, null, link)
+      e.preventDefault()
     })
-}
+  }
 
-const pageTitle = document.querySelector('.page-title');
-if (pageTitle) {
+  // Page Titles
+  const pageTitle = document.querySelector('.page-title');
   const pageTitleText = pageTitle.textContent;
   const headerTitle = document.querySelector('.header-title');
   const headingTitle = document.createElement('h1');
   headingTitle.textContent = pageTitleText;
   headerTitle.appendChild(headingTitle);
-}
 
-const contenth2 = document.querySelector('.details .page-title')
-if (contenth2) {
+  const contenth2 = document.querySelector('.details .page-title')
   const h2title = contenth2.innerHTML
   contenth2.innerHTML = `About ${h2title}`;
-}
 
-const siteActivities = document.querySelector('.site-activities')
-if (siteActivities) {
+  // Site activities list
+  const siteActivities = document.querySelector('.site-activities')
   const activitiesString = siteActivities.textContent;
   const activitiesArray = activitiesString.split(', ');
   const listElement = document.createElement('ul');
@@ -40,8 +40,6 @@ if (siteActivities) {
     listElement.appendChild(listItem);
   }
 
-  document.addEventListener("DOMContentLoaded", function(){
-    var elem = document.querySelector('.site-activities');
-    elem.parentNode.removeChild(elem);
-  });
-}
+  var elem = document.querySelector('.site-activities');
+  elem.parentNode.removeChild(elem);
+});
